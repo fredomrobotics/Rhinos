fafbe48a27715afa35eecff8085571f1cc044b83
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2024-03-16 13:32:57 -0600

    sonora

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -82,14 +82,49 @@ void RobotContainer::ConfigureButtonBindings() {
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
+  // Set up config for trajectory
+  frc::TrajectoryConfig config(AutoConstants::kMaxSpeed,
+                               AutoConstants::kMaxAcceleration);
+  // Add kinematics to ensure max speed is actually obeyed
+  config.SetKinematics(m_drive.kDriveKinematics);
+
+  // An example trajectory to follow.  All units in meters.
+  auto exampleTrajectory = frc::TrajectoryGenerator::GenerateTrajectory(
+      // Start at the origin facing the +X direction
+      frc::Pose2d{0_m, 0_m, 0_deg},
+      // Pass through these two interior waypoints, making an 's' curve path
+      {frc::Translation2d{1_m, 1_m}, frc::Translation2d{2_m, -1_m}},
+      // End 3 meters straight ahead of where we started, facing forward
+      frc::Pose2d{3_m, 0_m, 0_deg},
+      // Pass the config
+      config);
+
+  frc::ProfiledPIDController<units::radians> thetaController{
+      AutoConstants::kPThetaController, 0, 0,
+      AutoConstants::kThetaControllerConstraints};
+
+  thetaController.EnableContinuousInput(units::radian_t{-std::numbers::pi},
+                                        units::radian_t{std::numbers::pi});
+
+  frc2::SwerveControllerCommand<4> swerveControllerCommand(
+      exampleTrajectory, [this]() { return m_drive.GetPose(); },
+
+      m_drive.kDriveKinematics,
+
+      frc::PIDController{AutoConstants::kPXController, 0, 0},
+      frc::PIDController{AutoConstants::kPYController, 0, 0}, thetaController,
+
+      [this](auto moduleStates) { m_drive.SetModuleStates(moduleStates); },
+
+      {&m_drive});
+
+  // Reset odometry to the starting pose of the trajectory.
+  m_drive.ResetOdometry(exampleTrajectory.InitialPose());
+
+  // no auto
   return new frc2::SequentialCommandGroup(
-      frc2::InstantCommand([this]() { m_arm.waitForAngle(ArmConstants::kArmShootingAngle); }, {&m_arm}),
-      frc2::InstantCommand([this]() { m_launcher.autonomousShoot(); }, {&m_launcher}),
-      frc2::InstantCommand([this]() { m_arm.waitForAngle(ArmConstants::kArmIntakeAngle); }, {&m_arm}),
-      frc2::InstantCommand([this]() { m_drive.DriveForSeconds(-units::meters_per_second_t{-2}, 1.5); m_launcher.intakeForSeconds(2);}, {&m_drive, &m_launcher}),
-      frc2::InstantCommand([this]() { m_drive.DriveForSeconds(-units::meters_per_second_t{2}, 1.5);}, {&m_drive}),
-      frc2::InstantCommand([this]() { m_arm.waitForAngle(ArmConstants::kArmShootingAngle); }, {&m_arm}),
-      frc2::InstantCommand([this]() { m_launcher.autonomousShoot(); }, {&m_launcher}),
-      frc2::InstantCommand([this]() { m_drive.DriveForSeconds(-units::meters_per_second_t{-2}, 2.5);}, {&m_drive})
-  );
+      std::move(swerveControllerCommand),
+      frc2::InstantCommand(
+          [this]() { m_drive.Drive(0_mps, 0_mps, 0_rad_per_s, false, false); },
+          {}));
 }
